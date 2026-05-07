e8da0414697e5fd8ddfa95f8386e666254f8efad
Mohammad Alrefai <m.alrefai@proyoon.com>
2026-05-07 19:16:19 +0200

    Implement a REPL

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-         System.out.printf("$ ");
-
          // Capture the user's command in the "command" variable
         Scanner scanner = new Scanner(System.in);
-        String command = scanner.nextLine();
 
-        // Prints the "<command>: command not found" message
-        System.out.printf("%s: command not found%n", command);
+        while (true) {
+            System.out.printf("$ ");
+
+            String command = scanner.nextLine();
+            System.out.printf("%s: command not found%n", command);
+        }
     }
 }
