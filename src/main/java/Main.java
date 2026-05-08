7d91341e09fb87dbc444a3f16f810122501ce11d
Mohammad Alrefai <m.alrefai@proyoon.com>
2026-05-08 18:19:35 +0200

    Locate executable files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
-import java.util.Scanner;
+import java.io.File;
+import java.util.*;
+import java.util.function.Function;
 
 public class Main {
+    public static final Set<String> BUILTINS = Set.of("exit", "echo", "type");
+
     public static void main(String[] args) throws Exception {
          // Capture the user's command in the "command" variable
         Scanner scanner = new Scanner(System.in);
@@ -13,21 +17,38 @@ public class Main {
             String command = parts[0];
             String argument = (parts.length > 1) ? parts[1].strip() : "";
 
-            if(command.equals("exit"))
-                break;
+            if(command.equals("exit")) break;
 
-            System.out.println(switch (command) {
-                case "echo" -> argument;
-                case "type" -> evalType(argument);
-                default -> "%s: command not found".formatted(command);
-            });
+            Map<String, Function<String, String>> actions = Map.of(
+              "echo", cmd -> argument,
+              "type", cmd -> evalType(argument)
+            );
+
+            System.out.println(actions.getOrDefault(command, "%s: command not found"::formatted).apply(command));
         }
     }
 
     public static String evalType(String command) {
-        return switch (command) {
-          case "exit", "echo", "type" -> String.format("%s is a shell builtin", command);
-          default -> String.format("%s: not found", command);
-        };
+        File executable;
+        if (BUILTINS.contains(command)) {
+            return String.format("%s is a shell builtin", command);
+        } else if ((executable = findExecutable(command)) != null) {
+            return String.format("%s is %s", command, executable);
+        } else {
+            return String.format("%s: not found", command);
+        }
+    }
+
+    public static File findExecutable(String command) {
+        String pathEnv = System.getenv("PATH");
+        if (pathEnv == null) return null;
+
+        String[] paths = pathEnv.split(File.pathSeparator);
+        for(String path : paths) {
+            File file = new File(path, command);
+            if (file.isFile() && file.canExecute()) return file.getAbsoluteFile();
+        }
+
+        return null;
     }
 }
