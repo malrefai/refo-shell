d2ee281a5238e0154a7f32308f4e821e60203a65
Mohammad Alrefai <m.alrefai@proyoon.com>
2026-05-12 15:28:53 +0200

    Implement cd to handle absolute paths

--- src/main/java/com/shell/commands/CdCommand.java
+++ src/main/java/com/shell/commands/CdCommand.java
@@ -2,11 +2,27 @@ package com.shell.commands;
 
 import com.shell.state.ShellContext;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class CdCommand implements Command {
     @Override
     public String execute(List<String> args, ShellContext context) {
-        return "";
+        if (args.isEmpty()) {
+            return "";
+        }
+
+        String targetStr = args.getFirst();
+        Path targetPath = Paths.get(targetStr);
+
+        // Validate: Does it exist and is it a folder?
+        if (Files.isDirectory(targetPath)) {
+            context.setCurrentDirectory(targetPath);
+            return "";
+        } else {
+            return "cd: %s: No such file or directory".formatted(targetStr);
+        }
     }
 }
